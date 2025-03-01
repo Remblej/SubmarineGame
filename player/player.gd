5a51ee95f8666ebd7c3a0b065cef0c31415ac266
Artur Remblewski <remblej.outlook.com>
2025-03-01 21:04:09 +0100

    submarine graphic with movable drill

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,9 @@
 class_name Player extends CharacterBody2D
 
+@onready var drill_sprite: Sprite2D = $DrillSprite
 @onready var camera: Camera2D = $Camera2D
 @onready var movement: Movement = $Movement
 @onready var drill: Drill = $Drill
-@onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var cargo: Cargo = $Cargo
 @onready var hud: Hud = $CanvasLayer2/Hud
 @onready var upgrades_panel: Control = $CanvasLayer2/UpgradesPanel
@@ -14,13 +14,19 @@ func _ready() -> void:
 	Globals.drill_hit.connect(_on_drill_hit)
 	Globals.entered_base.connect(_on_entered_base)
 	Globals.exited_base.connect(_on_exited_base)
+	var drill_tween = get_tree().create_tween()
+	drill_tween.set_loops()
+	drill_tween.tween_property(drill_sprite, "offset:x", 10, .1)
+	drill_tween.tween_property(drill_sprite, "offset:x", 0, .2)
 
 func _process(delta: float) -> void:
+	# depth
 	var new_depth = round((global_position.y - 32) / 64)
 	if _depth != new_depth:
 		Globals.depth_changed.emit(new_depth)
 	_depth = new_depth
-	Globals.hull_integrity_changed.emit(.8, 1.0) # TODO
+	# drill animation
+	drill_sprite.offset.x = sin(delta*100000) * 100
 
 func _physics_process(delta: float) -> void:
 	rotation = movement.calculate_rotation(rotation,delta)
