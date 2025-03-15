542b3e1ad6cec79c2557f93cb49de9c9fbe8e7bf
Artur Remblewski <remblej.outlook.com>
2025-03-15 23:46:18 +0100

    tweening lights

--- mothership/mothership.gd
+++ mothership/mothership.gd
@@ -1,6 +1,6 @@
 class_name Mothership extends Node2D
 
-@export var speed = 150.0
+@export var target_coasting_speed = 75.0
 @export var traveling_alitutude = -64*6
 @export var descening_distance_forward = 64*14
 @export var descening_duration = 7
@@ -10,10 +10,20 @@ class_name Mothership extends Node2D
 @onready var bubble_vfx: CPUParticles2D = $BubbleVfx
 @onready var bubble_vfx_2: CPUParticles2D = $BubbleVfx2
 @onready var bubble_vfx_3: CPUParticles2D = $BubbleVfx3
+@onready var flash_light: PointLight2D = $FlashLight
+@onready var flash_light_2: PointLight2D = $FlashLight2
 
 var _is_traveling = true
+var _current_speed = target_coasting_speed
 var _is_descending = false
 var _is_ascending = false
+var _flash_light_enabled = false:
+	set(value):
+		if _flash_light_enabled != value:
+			_turn_flashlights(value)
+		_flash_light_enabled = value
+var _default_energy_fl_1: float
+var _default_energy_fl_2: float
 
 func _ready() -> void:
 	docking_area.monitoring = false
@@ -21,20 +31,27 @@ func _ready() -> void:
 	Globals.embark.connect(func(): Globals.game_state = Globals.GameState.MOTHERSHIP_DESCENDING)
 	docking_area.body_entered.connect(_on_player_enter)
 	position.y = traveling_alitutude
+	_default_energy_fl_1 = flash_light.energy
+	_default_energy_fl_2 = flash_light_2.energy
 
 func _physics_process(delta: float) -> void:
 	if _is_traveling:
-		position.x += speed * delta
+		position.x += _current_speed * delta
 
 func _on_game_state_change(_old: Globals.GameState, new: Globals.GameState):
-	_is_traveling = new in [Globals.GameState.MAIN_MENU, Globals.GameState.MOTHERSHIP_UI]
 	_is_descending = new == Globals.GameState.MOTHERSHIP_DESCENDING
 	_is_ascending = new == Globals.GameState.MOTHERSHIP_ASCENDING
 	_enable_bubble_vfx(_is_traveling || _is_ascending || _is_descending)
+	#_flash_light_enabled = _is_traveling || _is_ascending || _is_descending
 	_enable_docking_area(new == Globals.GameState.PLAYING)
 	if new == Globals.GameState.UNDOCKING:
 		_undock_player()
 	elif new == Globals.GameState.MOTHERSHIP_DESCENDING:
+		var ts = get_tree().create_tween()
+		ts.tween_property(self, "_current_speed", 200, 1).from(target_coasting_speed)
+		await ts.finished
+		_is_traveling = false
+
 		var target_pos = Vector2(position.x + descening_distance_forward, 0)
 		var tx = get_tree().create_tween()
 		tx.tween_property(self, "position:x", target_pos.x, descening_duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
@@ -57,6 +74,10 @@ func _on_game_state_change(_old: Globals.GameState, new: Globals.GameState):
 		tr.tween_property(self, "rotation_degrees", 0, descening_duration/2).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 		await tx.finished
 		Globals.game_state = Globals.GameState.MOTHERSHIP_UI
+		_is_traveling = true
+		var ts = get_tree().create_tween()
+		ts.tween_property(self, "_current_speed", target_coasting_speed, 1).from(200)
+		
 
 func _on_player_enter(body: Node2D):
 	if body is Player:
@@ -88,3 +109,12 @@ func _enable_bubble_vfx(enable: bool):
 	bubble_vfx.emitting = enable
 	bubble_vfx_2.emitting = enable
 	bubble_vfx_3.emitting = enable
+
+func _turn_flashlights(on: bool):
+	_turn_flashlight(flash_light, on, _default_energy_fl_1)
+	_turn_flashlight(flash_light_2,  on, _default_energy_fl_2)
+
+func _turn_flashlight(light: Light2D, on: bool, default_energy: float):
+	var t = get_tree().create_tween()
+	var target_energy = default_energy if on else 0.0
+	t.tween_property(light, "energy", target_energy, .2)
--- player/player.gd
+++ player/player.gd
@@ -8,10 +8,19 @@ class_name Player extends CharacterBody2D
 @onready var hud: Hud = $CanvasLayer2/Hud
 @onready var mother_ship_ui: Control = $CanvasLayer2/MotherShipUI
 @onready var bubble_vfx: CPUParticles2D = $BubbleVfx
+@onready var flash_light: PointLight2D = $FlashLight
+@onready var flash_light_2: PointLight2D = $FlashLight2
 
 var _states_for_bubble_vfx = [Globals.GameState.PLAYING, Globals.GameState.DOCKING, Globals.GameState.UNDOCKING]
 var _depth: int
 var _drill_tween: Tween
+var _flash_light_enabled = false:
+	set(value):
+		if _flash_light_enabled != value:
+			_turn_flashlights(value)
+		_flash_light_enabled = value
+var _default_energy_fl_1: float = .5
+var _default_energy_fl_2: float = .2
 
 func _ready() -> void:
 	Globals.tile_hit.connect(func (_resource: GatherableResource): movement.apply_recoil())
@@ -38,8 +47,17 @@ func _physics_process(delta: float) -> void:
 
 func _on_game_state_changed(old: Globals.GameState, new: Globals.GameState):
 	bubble_vfx.visible = new in _states_for_bubble_vfx
+	_flash_light_enabled = new in _states_for_bubble_vfx
 	if new == Globals.GameState.PLAYING:
 		_drill_tween.play()
 	elif old == Globals.GameState.PLAYING:
 		_drill_tween.pause()
-		
+
+func _turn_flashlights(on: bool):
+	_turn_flashlight(flash_light, on, _default_energy_fl_1)
+	_turn_flashlight(flash_light_2, on, _default_energy_fl_2)
+
+func _turn_flashlight(light: Light2D, on: bool, default_energy: float):
+	var t = get_tree().create_tween()
+	var target_energy = default_energy if on else 0.0
+	t.tween_property(light, "energy", target_energy, .2)
